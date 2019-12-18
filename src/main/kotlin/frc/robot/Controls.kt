426d6e8dc4aa1a494d79d8a7ac2ff1e2eb6c0576
Max Goldenson <47581749+GAM3TIN@users.noreply.github.com>
2019-12-18 12:38:31 -0800

    uwu

--- src/main/kotlin/frc/robot/Controls.kt
+++ src/main/kotlin/frc/robot/Controls.kt
@@ -12,8 +12,11 @@ import frc.robot.subsystems.superstructure.Elevator
 import org.ghrobotics.lib.commands.FalconCommand
 import org.ghrobotics.lib.commands.sequential
 import org.ghrobotics.lib.mathematics.units.derived.radians
+import org.ghrobotics.lib.mathematics.units.derived.velocity
+import org.ghrobotics.lib.mathematics.units.derived.volts
 import org.ghrobotics.lib.mathematics.units.inch
 import org.ghrobotics.lib.mathematics.units.inches
+import org.ghrobotics.lib.mathematics.units.meters
 import org.ghrobotics.lib.wrappers.FalconDoubleSolenoid
 import org.ghrobotics.lib.wrappers.FalconSolenoid
 import org.ghrobotics.lib.wrappers.hid.*
@@ -32,59 +35,61 @@ object Controls {
             // button(kB).changeOn(DriveSubsystem.followTrajectory(TestTrajectory.trajectory)).changeOn {
             // DriveSubsystem.odometry.resetPosition(TestTrajectory.trajectory.states[0].poseMeters)
             //}
-                //^^^ USE semi colin or comma to shorten ^^^
+            //^^^ USE semi colin or comma to shorten ^^^
             //TODO finish all button layouts and follow previos project.
             //top line is on press and bottom is when you let go of the button
-            state({ driverControllerLowLevel.getRawButton(10) }) {
-                button(kA).changeOn { ArmPresets.cargoLow() }
-               // button(kX).changeOn { ArmPresets.cargoMid() }
-                button(kX).changeOn { Vision.VisionGoToTarget() }
-                button(kY).changeOn { ArmPresets.cargoHigh() }
-                if(ArmPresets.hab3 or ArmPresets.hab2){
-                    button(kBumperLeft).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
-                    button(kBumperRight).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
-                }
-                else {
-                    button(kBumperLeft).changeOn { Intake.cargoIntake() }.changeOff { Intake.stop() }
-                    button(kBumperRight).changeOn { Intake.cargoOutake() }.changeOff { Intake.stop() }
-                }
-            }
-            state({ !driverControllerLowLevel.getRawButton(10) }) {
-                button(kA).changeOn { ArmPresets.hatchLow() }
-               // button(kX).changeOn { ArmPresets.hatchMid() }
-                button(kX).changeOn { Vision.VisionGoToTarget() }
-                button(kY).changeOn { ArmPresets.hatchHigh() }
-                if(ArmPresets.hab3 or ArmPresets.hab2){
-                    button(kBumperLeft).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(-1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
-                    button(kBumperRight).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(-1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
-                }
-                else {
-                    button(kBumperLeft).changeOn { Intake.hatchIntake() }.changeOff { Intake.stop() }
-                    button(kBumperRight).changeOn { Intake.hatchIntake() }.changeOff { Intake.stop() }
-                }
-            }
-            button(kB).changeOn { ArmPresets.stowed() }
-            pov(90).changeOn {ArmPresets.hab3Prep()}
-            pov(270).changeOn {ArmPresets.hab2Prep()}
-            pov(0).changeOn {ArmPresets.habYEET()}
-            pov(180).changeOn {ArmPresets.noHab()}
 
+            button(kA).whileOn { Elevator.master.setVelocity(0.5.meters.velocity) }
+//            state({ driverControllerLowLevel.getRawButton(10) }) {
+//                button(kA).changeOn { ArmPresets.cargoLow() }
+//               // button(kX).changeOn { ArmPresets.cargoMid() }
+//                button(kX).changeOn { Vision.VisionGoToTarget() }
+//                button(kY).changeOn { ArmPresets.cargoHigh() }
+//                if(ArmPresets.hab3 or ArmPresets.hab2){
+//                    button(kBumperLeft).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
+//                    button(kBumperRight).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
+//                }
+//                else {
+//                    button(kBumperLeft).changeOn { Intake.cargoIntake() }.changeOff { Intake.stop() }
+//                    button(kBumperRight).changeOn { Intake.cargoOutake() }.changeOff { Intake.stop() }
+//                }
+//            }
+//            state({ !driverControllerLowLevel.getRawButton(10) }) {
+//                button(kA).changeOn { ArmPresets.hatchLow() }
+//               // button(kX).changeOn { ArmPresets.hatchMid() }
+//                button(kX).changeOn { Vision.VisionGoToTarget() }
+//                button(kY).changeOn { ArmPresets.hatchHigh() }
+//                if(ArmPresets.hab3 or ArmPresets.hab2){
+//                    button(kBumperLeft).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(-1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
+//                    button(kBumperRight).changeOn { ClimbSubsystem.intakeWheels.setDutyCycle(-1.0) }.changeOff { ClimbSubsystem.intakeWheels.setNeutral() }
+//                }
+//                else {
+//                    button(kBumperLeft).changeOn { Intake.hatchIntake() }.changeOff { Intake.stop() }
+//                    button(kBumperRight).changeOn { Intake.hatchIntake() }.changeOff { Intake.stop() }
+//                }
+//            }
+//            button(kB).changeOn { ArmPresets.stowed() }
+//            pov(90).changeOn {ArmPresets.hab3Prep()}
+//            pov(270).changeOn {ArmPresets.hab2Prep()}
+//            pov(0).changeOn {ArmPresets.habYEET()}
+//            pov(180).changeOn {ArmPresets.noHab()}
+//
+//        }
+//
+//            val operatorWPIJoystick = XboxController(1)
+//            val operatorFalconXbox = operatorWPIJoystick.mapControls {
+//            }
         }
 
-            val operatorWPIJoystick = XboxController(1)
-            val operatorFalconXbox = operatorWPIJoystick.mapControls {
-            }
-
-
-            fun update() {
-                driverFalconXbox.update()
-                operatorFalconXbox.update()
-            }
+        fun update() {
+            driverFalconXbox.update()
+//                operatorFalconXbox.update()
         }
+    }
 
 //        private fun Command.andThen(block: () -> Unit) = sequential { +this@andThen; +InstantCommand(Runnable(block)) }
 //        fun update() {
 //            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 //        }
-    }
+}
 
