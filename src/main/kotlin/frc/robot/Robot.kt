f58c87aaa13acde86df85237da2a8f1b8c880dad
Matt <matthew.morley.ca@gmail.com>
2019-12-17 18:54:06 -0800

    s t a t e s p a c e

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -4,6 +4,7 @@ package frc.robot
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import frc.robot.subsystems.drive.DriveSubsystem
+import frc.robot.subsystems.superstructure.Elevator
 import frc.robot.subsystems.superstructure.controller.ElevatorController
 import org.ghrobotics.lib.wrappers.FalconTimedRobot
 
@@ -30,6 +31,8 @@ object Robot : FalconTimedRobot() {
 
     override fun robotPeriodic() {
         Controls.update()
+        Elevator.updateState()
+        Elevator.useState()
     }
 
     override fun disabledInit() {
--- src/main/kotlin/frc/robot/subsystems/superstructure/controller/ElevatorSSTest.kt
+++ src/main/kotlin/frc/robot/subsystems/superstructure/controller/ElevatorSSTest.kt
@@ -10,15 +10,17 @@ import frc.robot.subsystems.superstructure.Elevator
 import frc.robot.subsystems.superstructure.Superstructure
 import org.ejml.simple.SimpleMatrix
 import org.ghrobotics.lib.commands.FalconCommand
+import org.ghrobotics.lib.mathematics.units.*
 import org.ghrobotics.lib.mathematics.units.derived.volts
-import org.ghrobotics.lib.mathematics.units.inMeters
-import org.ghrobotics.lib.mathematics.units.inch
-import org.ghrobotics.lib.mathematics.units.inches
 import org.team5940.pantry.lib.WantedState
 
 class ElevatorSSTest: FalconCommand(Superstructure, Elevator) {
 
     private val constraints = TrapezoidProfile.Constraints(8.inches.inMeters(), 8.inches.inMeters())
+
+    override fun isFinished(): Boolean {
+        return false
+    }
 
     override fun execute() {
         // let's take the elevator up of it's down and down if it's up
@@ -32,20 +34,25 @@ class ElevatorSSTest: FalconCommand(Superstructure, Elevator) {
                 constraints, TrapezoidProfile.State(targetHeight.inMeters(), 0.0),
                 TrapezoidProfile.State(state.position.value, state.velocity.value)
         )
-        val target = profile.calculate(0.020)
-        ElevatorController.nextR = MatBuilder<N2, N1>(Nat.N2(), Nat.N1()).fill(target.position, target.velocity)
+
+        val target = profile.calculate(0.040)
+//        ElevatorController.nextR = MatBuilder<N2, N1>(Nat.N2(), Nat.N1()).fill(target.position, target.velocity)
+        ElevatorController.nextR = MatBuilder<N2, N1>(Nat.N2(), Nat.N1()).fill(targetHeight.inMeters(), 0.0)
 
         val y = ElevatorController.plant.calculateY(
                 MatBuilder<N2, N1>(Nat.N2(), Nat.N1()).fill(state.position.value, state.velocity.value),
                 ElevatorController.u)
 
-        ElevatorController.observer.correct(ElevatorController.controller.u, y)
+        ElevatorController.correct(y)
 
 //        ElevatorController.correct(MatBuilder<N2, N1>(Nat.N2(), Nat.N1()).fill(state.position.value, state.velocity.value))
         ElevatorController.predict(0.020)
 
         var voltage = ElevatorController.getU(0)
-        voltage = voltage.coerceIn(-2.0, 2.0)
+
+        println("error ${ElevatorController.error.get(0, 0).meters.inInches()} voltage: $voltage")
+
+        voltage = voltage.coerceIn(-8.0, 8.0)
         Elevator.wantsLowGear = false
         Elevator.wantedState = WantedState.Voltage(voltage.volts)
     }
