92313e20f2ab6441a99f3054f408525691d68e36
Matt <matthew.morley.ca@gmail.com>
2019-12-31 10:29:17 -0800

    uwu iwi owo it's a proximal

---
+++ src/main/kotlin/frc/robot/subsystems/superstructure/Proximal.kt
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.superstructure
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.InvertType
+import edu.wpi.first.wpilibj.controller.ArmFeedforward
+import edu.wpi.first.wpilibj.controller.PIDController
+import org.ghrobotics.lib.commands.FalconSubsystem
+import org.ghrobotics.lib.mathematics.units.SIUnit
+import org.ghrobotics.lib.mathematics.units.derived.Volt
+import org.ghrobotics.lib.mathematics.units.derived.radians
+import org.ghrobotics.lib.mathematics.units.derived.volts
+import org.ghrobotics.lib.mathematics.units.nativeunit.DefaultNativeUnitModel
+import org.ghrobotics.lib.mathematics.units.nativeunit.NativeUnitRotationModel
+import org.ghrobotics.lib.mathematics.units.nativeunit.nativeUnits
+import org.ghrobotics.lib.motors.ctre.falconSRX
+
+
+object Proximal: FalconSubsystem() {
+
+    val master = falconSRX(30, NativeUnitRotationModel(4096.nativeUnits * 9.33)) {
+        with(talonSRX) {
+            configFactoryDefault()
+            configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative)
+            setSensorPhase(true)
+        }
+        voltageCompSaturation = 12.volts
+    }
+
+    val follower = falconSRX(31, DefaultNativeUnitModel) {
+        with(talonSRX) {
+            configFactoryDefault()
+            setInverted(InvertType.OpposeMaster)
+        }
+        follow(master)
+    }
+
+    val position get() = master.encoder.position
+    val velocity get() = master.encoder.velocity
+
+    var voltageOutput: SIUnit<Volt>
+        get() = master.voltageOutput
+        set(value) {
+            master.setVoltage(value, 0.volts)
+        }
+}
---
+++ src/main/kotlin/frc/robot/subsystems/superstructure/ProximalCommands.kt
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.superstructure
+
+import edu.wpi.first.wpilibj.controller.ArmFeedforward
+import edu.wpi.first.wpilibj.controller.PIDController
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile
+import org.ghrobotics.lib.commands.FalconCommand
+import org.ghrobotics.lib.mathematics.units.SIUnit
+import org.ghrobotics.lib.mathematics.units.derived.*
+
+class ClosedLoopProximalMove(val targetPosition: SIUnit<Radian> = 0.radians): FalconCommand(Proximal) {
+
+    private val controller = ProfiledPIDController(25.0, 0.0, 0.48, constraints)
+
+    companion object {
+        private val feedforward = ArmFeedforward(0.3, 0.3, 3.9, 0.08)
+        private val constraints = TrapezoidProfile.Constraints(30.degrees.inRadians(), 60.degrees.inRadians())
+    }
+
+    override fun execute() {
+        val position = Proximal.position
+        Proximal.voltageOutput = controller.calculate(position.inRadians(), targetPosition.inRadians()).volts
+            + feedforward.calculate(Proximal.position.inRadians(), controller.setpoint.velocity)
+    }
+
+}
